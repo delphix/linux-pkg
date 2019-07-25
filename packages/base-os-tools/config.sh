1a8310a07f57e220ce7fa0cf3f75cba69f70ea2f
Pavel Zakharov <pavel.zakharov@delphix.com>
2019-07-25 14:50:00 -0400

    add build definition for base-os-tools

---
+++ packages/base-os-tools/config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright 2019 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/base-os-tools.git"
+DEFAULT_PACKAGE_VERSION=1.0.0
+
+function build() {
+	logmust dpkg_buildpackage_default
+}
