7899cf821fe451b6999c4f00398b220e42af5304
Prakash Surya <prakash.surya@delphix.com>
2019-08-05 14:33:27 -0700

    Use "github.com" hosted "drgn" repository

--- packages/drgn/config.sh
+++ packages/drgn/config.sh
@@ -16,7 +16,7 @@
 #
 
 # shellcheck disable=SC2034
-DEFAULT_PACKAGE_GIT_URL="https://gitlab.delphix.com/os-platform/drgn.git"
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/drgn.git"
 DEFAULT_PACKAGE_GIT_BRANCH="master"
 DEFAULT_PACKAGE_VERSION=1.0.0
 
