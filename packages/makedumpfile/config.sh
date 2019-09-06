b300a195bbeefe7012440051ce29abab053c3f50
Don Brady <don.brady@delphix.com>
2019-09-05 22:56:22 -0600

    DLPX-64156 crashdump should only include ZFS metadata

---
+++ packages/makedumpfile/config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Copyright 2019 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/makedumpfile.git"
+# Note: we get the package version programatically in our build() hook
+
+DEFAULT_PACKAGE_GIT_BRANCH="master"
+
+UPSTREAM_SOURCE_PACKAGE="makedumpfile"
+
+function prepare() {
+	logmust install_source_package_build_deps
+}
+
+function build() {
+	logmust cd "$WORKDIR/repo"
+	if [[ -z "$PACKAGE_VERSION" ]]; then
+		logmust eval PACKAGE_VERSION="$(grep '^VERSION=' Makefile | cut -d "=" -f 2)"
+	fi
+	logmust dpkg_buildpackage_default
+	# we only need makedumpfile package
+	logmust rm -f "$WORKDIR/artifacts/"kdump-tools_*_amd64.deb
+}
+
+function update_upstream() {
+	logmust update_upstream_from_source_package
+}
