dddecd1259d7221d185b5f2b854d41a6d261c359
John Gallagher <john.gallagher@delphix.com>
2019-10-14 13:26:46 -0700

    [Backport to 6.0] Output debug symbol (.ddeb) packages (#65)
    linux-pkg already creates .ddeb packages where appropriate, we just need
    to copy them to the artifacts/ directory so that they get uploaded with
    the rest of the packages by Jenkins at the end of the build.

--- lib/common.sh
+++ lib/common.sh
@@ -672,7 +672,7 @@ function dpkg_buildpackage_default() {
 	logmust set_changelog
 	logmust dpkg-buildpackage -b -us -uc
 	logmust cd "$WORKDIR/"
-	logmust mv ./*.deb artifacts/
+	logmust mv ./*deb artifacts/
 }
 
 #
