c25f5173a51c6b2f21a8997265603cc06ff4d5b0
Pavel Zakharov <pavel.zakharov@delphix.com>
2020-02-12 15:05:14 -0500

    [Backport to 6.0.0.1] Deprecating DEFAULT_PACKAGE_GIT_BRANCH
    Given our versioning model, where each package managed by linux-pkg
    is tagged and branched according to releases, we should rely on the
    DEFAULT_BRANCH parameter for fetching the source code for each package.
    
    Setting DEFAULT_PACKAGE_GIT_BRANCH is dangerous for 2 reasons:
    
     - First because a release tag for that repository then becomes
       misleading, as it is not what is used to rebuild that repository
       for a particular release version.
    
     - Second, if changes are later pushed to that branch, then rebuilding
       the original release will pull those new changes.
    
    For now, DEFAULT_PACKAGE_GIT_BRANCH is being deprecated rather than
    removed. We will evaluate whether it still has value for testing
    purposes or for projects such as SUV.

--- packages/crash-python/config.sh
+++ packages/crash-python/config.sh
@@ -17,7 +17,6 @@
 
 # shellcheck disable=SC2034
 DEFAULT_PACKAGE_GIT_URL="https://gitlab.delphix.com/os-platform/crash-python.git"
-DEFAULT_PACKAGE_GIT_BRANCH="next"
 DEFAULT_PACKAGE_VERSION=1.0.0
 
 function prepare() {
