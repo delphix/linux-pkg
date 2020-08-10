1c0eaa2a2dba6561667ba2a48f7d948e48d7201b
Paul Dagnelie <pcd@delphix.com>
2020-08-10 13:53:46 -0700

    Backport: Add grub and recovery environment commits to linux-pkg (#106)
    * Backport: Add grub2 package to the Delphix build process
    * Backport: Recov env package
    * Backport: route zfs package into recovery environment

---
+++ packages/recovery-environment/config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright 2020 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/recovery-environment.git"
+DEFAULT_PACKAGE_VERSION=1.0.0
+
+ZFS_DEB_PATH="$TOP/packages/zfs/tmp/artifacts"
+function prepare() {
+	if [ ! "$(ls -A "$ZFS_DEB_PATH")" ]; then
+		logmust "$TOP/buildpkg.sh" zfs
+	fi
+
+	logmust install_build_deps_from_control_file
+	logmust mkdir "$WORKDIR/repo/external-debs/"
+	logmust cp "$ZFS_DEB_PATH/"*.deb "$WORKDIR/repo/external-debs/"
+	return
+}
+
+function build() {
+	logmust dpkg_buildpackage_default
+}
