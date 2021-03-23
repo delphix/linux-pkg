0670ff8acac60a97cc9097e295bfef2f72c31ab7
Prakash Surya <prakash.surya@delphix.com>
2021-03-23 10:18:39 -0700

    Add comment to explain use of "--allow-downgrades"

--- lib/common.sh
+++ lib/common.sh
@@ -468,9 +468,22 @@ function create_workdir() {
 function install_pkgs() {
 	for attempt in {1..3}; do
 		echo "Running: sudo env DEBIAN_FRONTEND=noninteractive " \
-			"apt-get install -y $*"
+			"apt-get install -y --allow-downgrades $*"
+
+		#
+		# We use the "--allow-downgrades" for the case of a
+		# package needing to install the "unzip" debian package
+		# that we build via the "misc-debs" linux-pkg package.
+		# The "misc-debs" based "unzip" package may have an
+		# older version than what's already installed on the
+		# system, so we need to "--allow-downgrades" in order to
+		# install that specific package; further, that specific
+		# package is required to build the "virtualization"
+		# debian packages.
+		#
 		sudo env DEBIAN_FRONTEND=noninteractive apt-get install \
 			-y --allow-downgrades "$@" && return
+
 		echo "apt-get install failed, retrying."
 		sleep 10
 	done
