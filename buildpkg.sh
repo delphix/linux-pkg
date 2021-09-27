536cba2120bc11481feec250cb3f50df8389f58c
Pavel Zakharov <pavel.zakharov@delphix.com>
2021-09-27 17:50:50 -0400

    TOOL-12213 linux-pkg: run setup.sh automatically when needed (#194)

--- buildpkg.sh
+++ buildpkg.sh
@@ -19,6 +19,7 @@ TOP="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$TOP/lib/common.sh"
 
 logmust check_running_system
+logmust run_setup_if_needed
 
 function usage() {
 	[[ $# != 0 ]] && echo "$(basename "$0"): $*"
