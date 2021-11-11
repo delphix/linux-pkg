75085508bf349843a079c4ace3944bd0f8592f4e
Jules Tamagnan <jules.tamagnan@delphix.com>
2021-11-11 13:44:51 -0800

    GHM-187 [Backport of GHM-159 to 6.0/migration] Modify linux-pkg to point to GitHub version of repositories
    PR URL: https://www.github.com/delphix/linux-pkg/pull/202

--- packages/crash-python/config.sh
+++ packages/crash-python/config.sh
@@ -16,7 +16,7 @@
 #
 
 # shellcheck disable=SC2034
-DEFAULT_PACKAGE_GIT_URL="https://gitlab.delphix.com/os-platform/crash-python.git"
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/crash-python.git"
 DEFAULT_PACKAGE_VERSION=1.0.0
 
 function prepare() {
--- packages/crypt-blowfish/config.sh
+++ packages/crypt-blowfish/config.sh
@@ -16,7 +16,7 @@
 #
 # shellcheck disable=SC2034
 
-DEFAULT_PACKAGE_GIT_URL="https://gitlab.delphix.com/os-platform/linux-3rd-party-pkgs.git"
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/crypt-blowfish.git"
 DEFAULT_PACKAGE_VERSION="1.0.0"
 SKIP_COPYRIGHTS_CHECK=true
 
