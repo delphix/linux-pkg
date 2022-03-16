32a0315e8993eaf5be802355e901d1c5e99a889c
Sravya Meda <sravya.meda@delphix.com>
2022-03-15 20:32:58 -0400

    update copyright

--- packages/adoptopenjdk/config.sh
+++ packages/adoptopenjdk/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2018, 2020 Delphix
+# Copyright 2018, 2022 Delphix
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
