5e74090a1200d36ee23201f498ccc884fab75b96
Paul Dagnelie <pcd@delphix.com>
2022-09-22 14:59:39 -0700

    Handle query-packages on other platforms

--- packages/adoptopenjdk/config.sh
+++ packages/adoptopenjdk/config.sh
@@ -19,7 +19,7 @@
 DEFAULT_PACKAGE_GIT_URL=none
 PACKAGE_DEPENDENCIES="make-jpkg"
 
-case $(dpkg-architecture -q DEB_HOST_ARCH) in
+case $(dpkg-architecture -q DEB_HOST_ARCH 2>/dev/null || echo "none") in
 amd64)
 	_tarfile="OpenJDK8U-jdk_x64_linux_hotspot_8u345b01.tar.gz"
 	_tarfile_sha256="ed6c9db3719895584fb1fd69fc79c29240977675f26631911c5a1dbce07b7d58"
@@ -30,10 +30,8 @@ arm64)
 	_tarfile_sha256="c1965fb24dded7d7944e2da36cd902adf3b7b1d327aaa21ea507cff00a5a0090"
 	_jdk_path="/usr/lib/jvm/adoptopenjdk-java8-jdk-arm64"
 	;;
-*)
-	echo "Invalid architecture detected" >&2
-	exit 1
-	;;
+*) ;;
+
 esac
 
 function prepare() {
@@ -41,6 +39,10 @@ function prepare() {
 }
 
 function fetch() {
+	if [[ -z "$_tarfile" ]]; then
+		echo "Invalid architecture detected" >&2
+		exit 1
+	fi
 	logmust cd "$WORKDIR/"
 
 	local url="http://artifactory.delphix.com/artifactory/java-binaries/linux/jdk/8/$_tarfile"
@@ -49,6 +51,10 @@ function fetch() {
 }
 
 function build() {
+	if [[ -z "$_tarfile" ]]; then
+		echo "Invalid architecture detected" >&2
+		exit 1
+	fi
 	logmust cd "$WORKDIR/"
 
 	logmust env DEB_BUILD_OPTIONS=nostrip fakeroot make-jpkg "$_tarfile" <<<y
