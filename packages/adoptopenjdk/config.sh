62ca13d08f684bac3512af2b3ee41d687b49dca3
Paul Dagnelie <paul.dagnelie@delphix.com>
2022-09-23 11:22:51 -0700

    DLPX-83163 Add ARM support for adoptopenjdk package (#255)

--- packages/adoptopenjdk/config.sh
+++ packages/adoptopenjdk/config.sh
@@ -19,21 +19,36 @@
 DEFAULT_PACKAGE_GIT_URL=none
 PACKAGE_DEPENDENCIES="make-jpkg"
 
-if [[ "$UPSTREAM_PRODUCT_BRANCH" == "master" ]]; then
+case $(dpkg-architecture -q DEB_HOST_ARCH 2>/dev/null || echo "none") in
+amd64)
 	_tarfile="OpenJDK8U-jdk_x64_linux_hotspot_8u345b01.tar.gz"
 	_tarfile_sha256="ed6c9db3719895584fb1fd69fc79c29240977675f26631911c5a1dbce07b7d58"
 	_jdk_path="/usr/lib/jvm/adoptopenjdk-java8-jdk-amd64"
-else
-	_tarfile="OpenJDK8U-jdk_x64_linux_hotspot_8u345b01.tar.gz"
-	_tarfile_sha256="ed6c9db3719895584fb1fd69fc79c29240977675f26631911c5a1dbce07b7d58"
-	_jdk_path="/usr/lib/jvm/adoptopenjdk-java8-jdk-amd64"
-fi
+	;;
+arm64)
+	_tarfile="OpenJDK8U-jdk_aarch64_linux_hotspot_8u345b01.tar.gz"
+	_tarfile_sha256="c1965fb24dded7d7944e2da36cd902adf3b7b1d327aaa21ea507cff00a5a0090"
+	_jdk_path="/usr/lib/jvm/adoptopenjdk-java8-jdk-arm64"
+	;;
+*) ;;
+
+esac
 
 function prepare() {
 	logmust install_pkgs "$DEPDIR"/make-jpkg/*.deb
 }
 
 function fetch() {
+	# We exit here rather than above in the architecture detection logic
+	# to deal with the fact that this file can, during test runs, be
+	# sourced on platforms where builds are not happening. list-packages
+	# sources the file to gather information about the package, and this
+	# is performed on jenkins and macos during test runs. Having the exit
+	# occur above causes those runs to fail.
+	if [[ -z "$_tarfile" ]]; then
+		echo "Invalid architecture detected" >&2
+		exit 1
+	fi
 	logmust cd "$WORKDIR/"
 
 	local url="http://artifactory.delphix.com/artifactory/java-binaries/linux/jdk/8/$_tarfile"
@@ -42,6 +57,10 @@ function fetch() {
 }
 
 function build() {
+	if [[ -z "$_tarfile" ]]; then
+		echo "Invalid architecture detected" >&2
+		exit 1
+	fi
 	logmust cd "$WORKDIR/"
 
 	logmust env DEB_BUILD_OPTIONS=nostrip fakeroot make-jpkg "$_tarfile" <<<y
