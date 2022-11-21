89a430ac6122cad0e18424ed5a5419d230e77ab9
Sonam Kindy <sonam.kindy@delphix.com>
2022-11-21 17:10:52 -0600

    TOOL-16833 Update masking and containerized masking package builds to pull from dms-core-gate GitHub repo not Gitlab
    PR URL: https://www.github.com/delphix/linux-pkg/pull/267

--- packages/masking/config.sh
+++ packages/masking/config.sh
@@ -16,7 +16,7 @@
 #
 # shellcheck disable=SC2034
 
-DEFAULT_PACKAGE_GIT_URL="https://gitlab.delphix.com/masking/dms-core-gate.git"
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/dms-core-gate.git"
 PACKAGE_DEPENDENCIES="adoptopenjdk"
 
 function prepare() {
