d88941b278e8e649b84924d2987b52b5e9477aca
Jules Tamagnan <jules.tamagnan@delphix.com>
2023-01-12 09:42:57 -0800

    TOOL-17737 Update the virtualization package to point to GitHub
    PR URL: https://www.github.com/delphix/linux-pkg/pull/271

--- packages/virtualization/config.sh
+++ packages/virtualization/config.sh
@@ -16,7 +16,7 @@
 #
 # shellcheck disable=SC2034
 
-DEFAULT_PACKAGE_GIT_URL="https://gitlab.delphix.com/app/dlpx-app-gate.git"
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/dlpx-app-gate.git"
 PACKAGE_DEPENDENCIES="adoptopenjdk crypt-blowfish host-jdks"
 
 function prepare() {
