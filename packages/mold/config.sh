c2ab14e509082fa9fc8257630910df759c60ced4
Paul Dagnelie <pcd@delphix.com>
2023-08-28 16:53:24 -0700

    add mold pkg

---
+++ packages/mold/config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+# Copyright 2019, 2020 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/pcd1193182/mold.git"
+
+UPSTREAM_GIT_URL=https://salsa.debian.org/pkg-llvm-team/mold.git
+UPSTREAM_GIT_BRANCH=master
+
+function prepare() {
+	logmust install_build_deps_from_control_file
+}
+
+function build() {
+	logmust dpkg_buildpackage_default
+}
+
+function update_upstream() {
+	logmust update_upstream_from_git
+}
