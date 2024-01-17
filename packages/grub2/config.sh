91ae3d6ed2f85d381463a9c94feb1cb2318040c6
Prakash Surya <prakash.surya@delphix.com>
2024-01-17 09:47:09 -0800

    Revert "TOOL-24368 Use grub packages provided by Ubuntu"
    This reverts commit bf6e956e39935f470793f42bd024b30a3f353286.

---
+++ packages/grub2/config.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Copyright 2020 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/grub2"
+
+UPSTREAM_GIT_URL=https://git.launchpad.net/ubuntu/+source/grub2
+UPSTREAM_GIT_BRANCH="applied/ubuntu/${UBUNTU_DISTRIBUTION}-updates"
+
+SKIP_COPYRIGHTS_CHECK=true
+
+#
+# Install build dependencies for the package.
+#
+function prepare() {
+	logmust install_build_deps_from_control_file
+}
+
+#
+# Build the package.
+#
+function build() {
+	logmust dpkg_buildpackage_default
+}
+
+#
+# Hook to fetch upstream package changes and merge into our tree.
+#
+function update_upstream() {
+	logmust update_upstream_from_git
+}
