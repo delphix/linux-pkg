070b1219f3561295ba61089360304d59b2278a34
Peter Jang <peter.jang@delphix.com>
2024-06-05 12:09:05 -0400

    DLPX-91307 Add Java 17 provided by Ubuntu to the appliance

---
+++ packages/openjdk-17-jdk/config.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024 Delphix
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# shellcheck disable=SC2034
+
+UPSTREAM_SOURCE_PACKAGE="openjdk-17"
+
+DEFAULT_PACKAGE_GIT_URL="https://github.com/pjang-dlpx/openjdk-17"
+
+function prepare() {
+        logmust install_build_deps_from_control_file
+}
+
+function build() {
+        logmust dpkg_buildpackage_default
+}
+
+function update_upstream() {
+        logmust update_upstream_from_source_package
+}
