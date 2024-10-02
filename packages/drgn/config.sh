69f6bd4884e2fe6dda0934ac4cd17c3a56e7ad12
Palash Gandhi <87093175+palash-gandhi@users.noreply.github.com>
2024-10-02 10:36:09 -0700

    DLPX-94086 LTS 24.04: update linux-pkg for Ubuntu 24.04 appliance
    PR URL: https://www.github.com/delphix/linux-pkg/pull/349

--- packages/drgn/config.sh
+++ packages/drgn/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Copyright 2019, 2020 Delphix
+# Copyright 2019, 2025 Delphix
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
