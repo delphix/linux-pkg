493a9726fb2741c82d19e649885e0a69d0da1db4
Prakash Surya <prakash.surya@perforce.com>
2025-01-09 08:39:44 -0800

    DLPX-93066 zfs package build failed at cargo-bundle-licenses v2.2.0 compile step (#331)

--- packages/zfs/config.sh
+++ packages/zfs/config.sh
@@ -56,7 +56,7 @@ function prepare() {
 		zlib1g-dev
 	logmust install_kernel_headers
 	logmust install_pkgs "$DEPDIR"/delphix-rust/*.deb
-	logmust cargo install cargo-bundle-licenses
+	logmust cargo install cargo-bundle-licenses@2.1.1 --locked
 	logmust install_pkgs "$DEPDIR"/delphix-go/*.deb
 	logmust install_pkgs "$DEPDIR"/dwarves/*.deb
 }
