70a78ae46808db119fc0c55e3aad62d1ebaafb05
Prakash Surya <prakash.surya@perforce.com>
2025-08-01 18:00:25 -0700

    DLPX-94999 linux-kernel updates to 6.14 are failing
    PR URL: https://www.github.com/delphix/linux-pkg/pull/364

--- default-package-config.sh
+++ default-package-config.sh
@@ -99,9 +99,14 @@ function kernel_build() {
 	# Canonical's releases and our releases.
 	#
 	local canonical_abinum delphix_abinum kernel_release kernel_version
+
 	canonical_abinum=$(fakeroot debian/rules printenv | grep -E '^abinum ' | cut -d= -f2 | tr -d '[:space:]')
 	delphix_abinum="${canonical_abinum}-$(date -u +"dx%Y%m%d%H")-$(git rev-parse --short HEAD)"
+
 	kernel_release=$(fakeroot debian/rules printenv | grep -E '^release ' | cut -d= -f2 | tr -d '[:space:]')
+	if [[ -z "$kernel_release" ]]; then
+		kernel_release=$(fakeroot debian/rules printenv | grep -E '^DEB_VERSION_UPSTREAM ' | cut -d= -f2 | tr -d '[:space:]')
+	fi
 
 	#
 	# We record the kernel version into a file. This field is consumed
