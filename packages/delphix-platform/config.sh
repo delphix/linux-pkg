363381f33156e3826e927094d6eae3173b19e5d8
Sebastien Roy <sebastien.roy@perforce.com>
2026-02-10 18:58:48 -0500

    fix

--- packages/delphix-platform/config.sh
+++ packages/delphix-platform/config.sh
@@ -18,9 +18,14 @@
 
 DEFAULT_PACKAGE_GIT_URL="https://github.com/delphix/delphix-platform.git"
 
+function prepare() {
+	logmust cd "$WORKDIR/repo"
+	logmust sudo make build-deps
+}
+
 function build() {
 	logmust cd "$WORKDIR/repo"
-	logmust sudo make packages VERSION="1.0.0-$PACKAGE_REVISION"
+	logmust make packages VERSION="1.0.0-$PACKAGE_REVISION"
 	logmust sudo chown -R "$USER:" artifacts
 	logmust mv artifacts/*deb "$WORKDIR/artifacts/"
 }
